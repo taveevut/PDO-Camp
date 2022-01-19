b062ca0dcbfdb67c6be96ed4cb26e3e61af212fd
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-19 14:25:05 +0700

    fix: สร้างหน้าเพิ่มข้อมูลสมาชิก

---
+++ create.php
@@ -0,0 +1,69 @@
+<?php include('database.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible"
+         content="IE=edge">
+   <meta name="viewport"
+         content="width=device-width, initial-scale=1.0">
+   <title>สร้างรายการสมาชิก</title>
+</head>
+
+<body>
+   <h1>สร้างรายการ </h1>
+
+   <form method="POST"
+         action="process.php"
+         class="rendered-form">
+
+         <input type="hidden" name="action" value="create">
+         
+      <div class="formbuilder-text">
+         <label>ชื่อผู้ใช้งาน</label><br>
+         <input type="text"
+                name="username"
+                required="required"
+                aria-required="true">
+      </div>
+      <div class="formbuilder-text">
+         <label>รหัสผ่าน</label><br>
+         <input type="password"
+                name="password"
+                required="required"
+                aria-required="true">
+      </div>
+      <div class="formbuilder-text">
+         <label>ชื่อ</label><br>
+         <input type="text"
+                name="name"
+                required="required"
+                aria-required="true">
+      </div>
+      <div class="formbuilder-text">
+         <label>สกุล</label><br>
+         <input type="text"
+                name="surname"
+                required="required"
+                aria-required="true">
+      </div>
+      <div class="formbuilder-text">
+         <label>ชื่อเล่น</label><br>
+         <input type="text"
+                name="nickname">
+      </div>
+      <div class="formbuilder-text">
+         <label>เบอร์โทรศัพท์</label><br>
+         <input type="tel"
+                name="tel">
+      </div>
+      <div class="formbuilder-text"><br>
+         <button type="submit">บันทึกข้อมูล</button>
+         <button type="reset">ยกเลิก</button>
+      </div>
+   </form>
+</body>
+
+</html>
