f44b99368a95445b512fd86e78c59413b48d7015
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-19 13:28:00 +0700

    delete

--- select_all.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('./database.php');
-
-$stmt = $db_con->prepare("SELECT * FROM members ORDER BY id DESC");
-$stmt->execute();
-while ($rows = $stmt->fetch(PDO::FETCH_ASSOC)) {
-  echo ("{$rows['id']}-{$rows['name']} <br>");
-}
