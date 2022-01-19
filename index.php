7e2348303c8cde1e8ae4c48730f4d2398caf4039
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-19 14:55:41 +0700

    fix: all(*)

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
             <td><?php echo $rows['created_at']; ?></td>
             <td>
                <a href="#">ดูรายการ</a>&nbsp;
-               <a href="#">แก้ไขรายการ</a>&nbsp;
+               <a href="./edit.php?id=<?php echo $rows['id']; ?>">แก้ไขรายการ</a>&nbsp;
                <a href="./process.php?action=DELETE&id=<?php echo $rows['id']; ?>" onclick="return confirm('คุณต้องการลบรายนี้ใช่หรือไม่ !')">ลบรายการ</a>&nbsp;
             </td>
          </tr>
