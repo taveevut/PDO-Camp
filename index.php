652f8b6206cb0907f4c4a06a51be632d34a70b1e
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-19 13:26:10 +0700

    สร้าง interface แสดงข้อมูลรายการสมาชิก

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible"
+         content="IE=edge">
+   <meta name="viewport"
+         content="width=device-width, initial-scale=1.0">
+   <title>แสดงรายการสมาชิก</title>
+</head>
+
+<body>
+   <h1>ข้อมูลรายการสมาชิก</h1>
+   <table border="1" cellpadding="5">
+      <tr>
+         <td>ชื่อผู้ใช้งาน</td>
+         <td>รหัสผ่าน</td>
+         <td>Vernon Hill</td>
+         <td>ชื่อเล่น</td>
+         <td>เบอร์โทรศัพท์</td>
+         <td>วันที่สร้าง</td>
+      </tr>
+      <tr>
+         <td>select</td>
+         <td>7d841c2b-40b5-53a8-8fdc-573273722345</td>
+         <td>Lettie Garrett</td>
+         <td>important</td>
+         <td>8Oz5A2</td>
+         <td>8/15/2114</td>
+      </tr>
+      <tr>
+         <td>east</td>
+         <td>8bd2b120-5569-526d-9ee1-bd6a53946f20</td>
+         <td>Anne Schultz</td>
+         <td>ชื่อเล่น</td>
+         <td>vXsb8kWyvc2</td>
+         <td>2/26/2053</td>
+      </tr>
+   </table>
+</body>
+
+</html>
