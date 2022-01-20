c9bc81a75284708eaf3cd9d80bd159f5a19edcb9
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-20 13:32:27 +0700

    fix: แยกส่วน

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -8,7 +8,8 @@
    </div>
    <ul class="app-menu">
       <li><a class="app-menu__item" href="dashboard.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
-      <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">เว็บบอร์ด</span></a></li>
+      <li><a class="app-menu__item" href="./webboard"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">เว็บบอร์ด</span></a></li>
+      <li><a class="app-menu__item" href="./member"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">สมาชิก</span></a></li>
       <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i class="treeview-indicator fa fa-angle-right"></i></a>
          <ul class="treeview-menu">
             <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
