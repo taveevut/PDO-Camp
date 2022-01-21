83358bae75d7aa5edc8cff5838558b8af34a40f6
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-21 11:13:34 +0700

    fix: ดึงข้อมูลมาแสดงในหน้าแรก

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<header class="app-header"><a class="app-header__logo" href="#">SKRU ​App</a>
+   <!-- Navbar Right Menu-->
+   <ul class="app-nav">
+      <li class="app-search">
+         <input class="app-search__input" type="search" placeholder="Search">
+         <button class="app-search__button"><i class="fa fa-search"></i></button>
+      </li>
+      <!-- User Menu-->
+      <li>
+         <a class="app-nav__item" href="./login"><i class="fa fa-user fa-lg"></i></a>
+      </li>
+   </ul>
+</header>
