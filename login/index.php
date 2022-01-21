6056358fef4d78ba04ca1c3b304938df139a2120
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-21 14:03:39 +0700

    fix: custom styles

--- login/index.php
+++ login/index.php
@@ -22,6 +22,8 @@
    <link rel="stylesheet" type="text/css" href="/skru-pdo-camp/assets/plugins/vali-admin/css/main.css">
    <!-- Font-icon css-->
    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   <link rel="stylesheet" type="text/css" href="/skru-pdo-camp/assets/css/styles.css">
 </head>
 
 <body>
--- member/templates/head.php
+++ member/templates/head.php
@@ -23,8 +23,8 @@
    <!-- Font-icon css-->
    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
-
    <!-- include summernote css/js -->
    <link href="/skru-pdo-camp/assets/plugins/summernote/summernote-bs4.min.css" rel="stylesheet">
+
+   <link rel="stylesheet" type="text/css" href="/skru-pdo-camp/assets/css/styles.css">
 </head>
