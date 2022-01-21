d7223ec55cff172a58d338c46104804c1a940a93
Taveevut Nakomah <nakomah.web@gmail.com>
2022-01-22 05:15:33 +0700

    fix: เพิ่ม function

--- helpers/url_helper.php
+++ helpers/url_helper.php
@@ -1,4 +1,32 @@
 <?php
+if (!function_exists('site_assets')) {
+   function site_assets($url = '')
+   {
+      return ROOT_PROJECT . "/assets/" . $url;
+   }
+}
+
+if (!function_exists('site_node_modules')) {
+   function site_node_modules($url = '')
+   {
+      return ROOT_PROJECT . "/node_modules/" . $url;
+   }
+}
+
+if (!function_exists('site_uploads')) {
+   function site_uploads($url = '')
+   {
+      return ROOT_PROJECT . "/uploads/" . $url;
+   }
+}
+
+if (!function_exists('site_admin_url')) {
+   function site_admin_url($url = '')
+   {
+      return ROOT_PROJECT . "/administrator/" . $url;
+   }
+}
+
 if (!function_exists('redirect_admin')) {
    function redirect_admin($url = '')
    {
